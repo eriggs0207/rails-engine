df91795adfb8a33283e8a8ce75a4b9ba52033423
Erik Riggs <riggs.erik@gmail.com>
2022-10-18 15:01:44 -0600

    feat/test: create endpoint 5: item by id

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -18,4 +18,20 @@ describe "Merchants API" do
     expect(items[:data][2][:attributes][:merchant_id]).to be_a(Integer)
     expect(items[:data][2][:attributes][:unit_price]).to be_a(Float)
   end
-end 
+
+  it "can get one item by its id" do
+    item = create(:item)
+
+    get api_v1_item_path(item)
+
+    expect(response).to be_successful
+
+    item = JSON.parse(response.body, symbolize_names: true)
+
+    expect(item[:data]).to have_key(:id)
+    expect(item[:data]).to have_key(:attributes)
+    expect(item[:data][:attributes][:name]).to be_a(String)
+    expect(item[:data][:attributes][:merchant_id]).to be_a(Integer)
+    expect(item[:data][:attributes][:unit_price]).to be_a(Float)
+  end
+end
